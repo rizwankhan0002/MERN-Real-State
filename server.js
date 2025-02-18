a21c2fbda4623b904b9187756ae07ea1151d9081
rizwankhan0002 <rizwankhan92748@gmail.com>
2025-02-18 17:46:20 +0500

    create a test api route

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 import express from 'express'
+import 'dotenv/config'
+import connectDB from './Server/config/mongodb.js'
+import userRouter from './Server/config/routes/userRouter.js'
 
 //App Config
 const app = express()
 const port = process.env.PORT || 5000
+connectDB()
+
+app.use('/api/user', userRouter)
 
 app.listen(port, () => console.log('Server started on PORT : '+ port))
