9c0d50e43d66434f2e9acef04e205bf948da1cec
rizwankhan0002 <rizwankhan92748@gmail.com>
2025-03-02 16:51:29 +0500

    create signin API Route

--- Server/config/routes/authRouter.js
+++ Server/config/routes/authRouter.js
@@ -1,8 +1,8 @@
 import express from 'express'
-import { signup } from '../controllers/authController.js'
+import { signin, signup } from '../controllers/authController.js'
 
 const authRouter = express.Router()
 
 authRouter.post('/signup', signup)
-
+authRouter.post('/signin', signin)
 export default authRouter
