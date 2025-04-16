3e154e26ab91d58af45a47456559da569973ab12
rizwankhan0002 <rizwankhan92748@gmail.com>
2025-04-17 00:39:26 +0500

    complete delete user listing functionality

--- Server/config/controllers/listingController.js
+++ Server/config/controllers/listingController.js
@@ -1,4 +1,5 @@
 import Listing from "../models/listingModel.js"
+import { errorHandler } from "../utils/error.js"
 
 
 const createListing = async (req, res, next) => {
@@ -10,4 +11,22 @@ try {
 }
 }
 
-export { createListing }
+const deleteListing = async (req, res, next) => {
+   const listing = await Listing.findById(req.params.id)
+
+   if (!listing) {
+    return next(errorHandler(400, 'Listing not found'))
+   }
+
+   if (req.user.id !== listing.userRef) {
+   return next(errorHandler(400, 'You can only delete your own listings!'))
+   }
+   try {
+    await Listing.findByIdAndDelete(req.params.id)
+    res.status(200).json('Listing has been deleted')
+   } catch (error) {
+    next(error)
+   }
+}
+
+export { createListing, deleteListing }
--- Server/config/routes/listingRouter.js
+++ Server/config/routes/listingRouter.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { createListing } from '../controllers/listingController.js'
+import { createListing, deleteListing } from '../controllers/listingController.js'
 import authMiddleware from '../../middleware/authmiddlerware.js'
 
 const listRouter = express.Router()
 
 listRouter.post('/create', authMiddleware, createListing)
+listRouter.delete('/delete/:id', authMiddleware, deleteListing)
 
 export default listRouter
